949992c64fd9486b273b6195493308e22ad81591
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-05-02 11:38:33 +0900

    add: new imageliteral file(#3)

---
+++ Just-A-Guide/Just-A-Guide/Global/Literal/ImageLiteral.swift
@@ -0,0 +1,24 @@
+//
+//  ImageLiteral.swift
+//  Just-A-Guide
+//
+//  Created by SHIN YOON AH on 2022/05/02.
+//
+
+import SwiftUI
+
+enum ImageLiteral {
+    
+    // MARK: - button
+    
+    static var btnSample: Image = Image("img_lock")
+    
+    // MARK: - image
+    
+    static var btnSample2: Image = Image("img_lock")
+    
+    // MARK: - icon
+    
+    static var btnSample3: Image = Image("img_lock")
+    
+}
