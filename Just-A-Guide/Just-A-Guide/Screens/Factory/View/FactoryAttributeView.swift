999796072680075ad9c32e1d6a06684ecf4af58c
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-05-04 09:29:35 +0900

    add: configurate adding attribute button on page sheet(#5)

--- Just-A-Guide/Just-A-Guide/Screens/Factory/View/FactoryAttributeView.swift
+++ Just-A-Guide/Just-A-Guide/Screens/Factory/View/FactoryAttributeView.swift
@@ -11,15 +11,33 @@ struct FactoryAttributeView: View {
     var body: some View {
         ZStack {
             Color.gray100
+            
+            addingAttributeButton
         }
-        .border(.ultraThinMaterial, width: 1)
+        .border(.ultraThickMaterial, width: 1)
         .cornerRadius(20, corners: [.topLeft, .topRight])
         .ignoresSafeArea(.all)
     }
 }
 
 extension FactoryAttributeView {
-
+    private var addingAttributeButton: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    print("present adding attribute view")
+                }, label: {
+                    Image(systemName: "plus.circle")
+                        .foregroundColor(.gray600)
+                        .imageScale(.large)
+                })
+                .padding()
+            }
+            
+            Spacer()
+        }
+    }
 }
 
 struct FactoryAttributeView_Previews: PreviewProvider {
